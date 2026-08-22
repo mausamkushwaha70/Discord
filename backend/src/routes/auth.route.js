abe34d9c9e0326516a0d9a0f6a0da5bce043ed8f
mausam <mausamkkushwaha@gmail.com>
2026-08-22 19:53:32 +0530

    feat(routes): add user authentication register and login routes

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,7 +1,11 @@
 import express from "express"
+import {upload} from "../config/multer.js"
+import { userLoginController, userRegisterController } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
+router.post("/user-register",upload.single("image"), userRegisterController)
+router.post("/user-login", userLoginController)
 
 
 export default router;
