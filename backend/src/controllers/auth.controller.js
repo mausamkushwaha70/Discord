6bc183f2e27e47b972cf2d92f01e6effa5af6359
mausam <mausamkkushwaha@gmail.com>
2026-08-27 19:21:57 +0530

    feat: add forgetpassword functionality

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -4,8 +4,7 @@ import bcrypt from "bcrypt";
 import { tokenGenerate } from "../utils/token.utils.js";
 import redis from "../config/redis.config.js";
 import { generateOTP } from "../utils/otp.utill.js";
-
-
+import { sendEmail } from "../services/email.service.js";
 
 export const userRegisterController = async (req, res) => {
   try {
@@ -80,7 +79,8 @@ export const userLoginController = async (req, res) => {
     });
   }
 
-  const isExist = await userModel.findOne({ email });
+  const isExist = await userModel.findOne({ email }).select("password")
+  console.log(isExist)
 
   if (!isExist) {
     return res.status(404).json({
@@ -135,22 +135,51 @@ export const userLoginController = async (req, res) => {
 
 export const googleAuthcontroller = async (req, res) => {
   try {
-    const {email, name, given_name, picture, sub } = req.user._json;
-  console.log(req.user);
-  const user = await userModel.findOne({ email });
+    const { email, name, given_name, picture, sub } = req.user._json;
+    console.log(req.user);
+    const user = await userModel.findOne({ email });
 
-  if (user) {
-    if (!user.googleId) {
-      user.googleId = sub;
-      await user.save();
+    if (user) {
+      if (!user.googleId) {
+        user.googleId = sub;
+        await user.save();
+      }
+
+      const accessToken = tokenGenerate(user._id, "20min");
+      const refreshToken = tokenGenerate(user._id, "2d");
+
+      res.cookie("accessToken", accessToken, {
+        httpOnly: true,
+        maxAge: 20 * 60 * 1000,
+      });
+
+      res.cookie("refreshToken", refreshToken, {
+        httpOnly: true,
+        maxAge: 2 * 24 * 60 * 60 * 1000,
+      });
+
+      return res.status(200).json({
+        success: true,
+        message: "user loggedin successfully",
+        user,
+      });
     }
 
-    const accessToken = tokenGenerate(user._id, "20min");
-    const refreshToken = tokenGenerate(user._id, "2d");
+    const newUser = await userModel.create({
+      username: given_name,
+      fullName: name,
+      email,
+      profile_pic: picture,
+      googleId: sub,
+      authProvider: req.user.provider,
+    });
+
+    const accessToken = generateToken(newUser._id, "15min");
+    const refreshToken = generateToken(newUser._id, "2d");
 
     res.cookie("accessToken", accessToken, {
       httpOnly: true,
-      maxAge: 20 * 60 * 1000,
+      maxAge: 15 * 60 * 1000,
     });
 
     res.cookie("refreshToken", refreshToken, {
@@ -158,108 +187,105 @@ export const googleAuthcontroller = async (req, res) => {
       maxAge: 2 * 24 * 60 * 60 * 1000,
     });
 
-    return res.status(200).json({
+    return res.status(201).json({
       success: true,
-      message: "user loggedin successfully",
-      user,
+      message: "user register successfully",
+      newUser,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      message: "internal server error",
+      error: error.message,
     });
   }
-
-  const newUser = await userModel.create({
-    username:given_name,
-    fullName:name,
-    email,
-    profile_pic:picture,
-    googleId:sub,
-    authProvider:req.user.provider
-  })
-
-  const accessToken = generateToken(newUser._id,"15min")
-    const refreshToken = generateToken(newUser._id,"2d")
-
-
-    res.cookie("accessToken",accessToken,{
-        httpOnly:true,
-        maxAge:15*60*1000
-    })
-
-    res.cookie("refreshToken",refreshToken,{
-        httpOnly:true,
-        maxAge:2*24*60*60*1000
-    })
-
-    return res.status(201).json({
-        success:true,
-        message:"user register successfully",
-        newUser
-    })
-  } catch (error) {
-    console.log(error)
-      return res.status(500).json({
-        success:false,
-        message:"internal server error",
-        error:error.message
-      })
-    
-  }
-
 };
 
-export const userLogoutController = async (req,res)=>{
+export const userLogoutController = async (req, res) => {
   // before using redis three credentials is required
   // host, port, password
 
   try {
-    const {refreshToken,accessToken}= req.cookies;
-  if(accessToken){
-    await redis.set(`Bearer: accessToken: ${accessToken}`, "true")
-  }
+    const { refreshToken, accessToken } = req.cookies;
+    if (accessToken) {
+      await redis.set(`Bearer: accessToken: ${accessToken}`, "true");
+    }
 
-  if(refreshToken){
-    await redis.set(`Bearer: refreshToken: ${refreshToken}`, "true")
-  }
-  // await redis.set(`tokenblackelisted${accessToken}`,true)
+    if (refreshToken) {
+      await redis.set(`Bearer: refreshToken: ${refreshToken}`, "true");
+    }
+    // await redis.set(`tokenblackelisted${accessToken}`,true)
 
-  res.clearCookie("accessToken")
-  res.clearCookie("refreshToken")
+    res.clearCookie("accessToken");
+    res.clearCookie("refreshToken");
 
-  return res.status(200).json({
-    success:true,
-    message:"logout successfully"
-  })
+    return res.status(200).json({
+      success: true,
+      message: "logout successfully",
+    });
   } catch (error) {
     return res.status(500).json({
-    success:false,
-    error:error.message
-  })
+      success: false,
+      error: error.message,
+    });
   }
-}
+};
 
-export const forgatePasswordController = async (req,res)=>{
-  const {email} =req.body
+export const forgatePasswordController = async (req, res) => {
+  try {
+    const { email } = req.body;
 
-  if(!email){
+  if (!email) {
     return res.status(400).json({
-      success:false,
-      message:"email not found"
-    })
+      success: false,
+      message: "email not found",
+    });
   }
 
-  const user = await userModel.findOne({email})
+  const user = await userModel.findOne({ email })
 
-  if(!user) return res.status(404).json({
-    success:false,
-    message:"user not found"
-  })
+  if (!user)
+    return res.status(404).json({
+      success: false,
+      message: "user not found",
+    });
 
   const OTP = generateOTP();
 
-  const hashedOPT = bcrypt.hashSync(OTP,10)
+  const hashedOPT = bcrypt.hashSync(OTP, 10);
 
-  await redis.set(`reset-password-hashed-${email}`,hashedOPT,"EX",5*60)
+  await redis.set(`reset-password-hashed-${email}`, hashedOPT, "EX", 5 * 60);
 
-  
+  await sendEmail(
+    user.email,
+    "Reset your app password",
+    `Do not share with anyone. This ${OTP} OTP message is only valid upto 5 minutes `,
+    `<div style="font-family: Arial, sans-serif">
+      <h2>Password Reset Request</h2>
 
-  
+      <p>Your OTP for resetting your password is:</p>
 
-}
+      <h1 style="letter-spacing: 5px;">${OTP}</h1>
+
+      <p>
+        This OTP will expire in <strong>10 minutes</strong>.
+      </p>
+
+      <p>If you did not request a password reset, please ignore this email.</p>
+    </div>`,
+  );
+
+  return res.status(200).json({
+    success:true,
+    message:"otp send sucessfully"
+  })
+  } catch (error) {
+    console.log(error)
+    return res.status(500).json({
+      success:false,
+      message:"Internal server error",
+      error:error.message
+    })
+  }
+};
