b4e3dacfa7da9c7cf265fd7a5d2a8eead931d66b
mausam <mausamkkushwaha@gmail.com>
2026-08-27 19:55:40 +0530

    feat: add user-find functionality

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,6 +1,6 @@
 import userModel from "../models/user.model.js";
 
-export const getMeController = async (req,res) => {
+export const getMeController = async (req, res) => {
     try {
         const user = await userModel.findById(req.user.id).select("     password")
 
@@ -14,13 +14,40 @@ export const getMeController = async (req,res) => {
         return res.status(200).json({
             success: true,
             message: "fetched myProfile",
-            data:user
+            data: user
         });
     } catch (error) {
         console.log(error);
         return res.status(500).json({
             success: false,
             message: "Internal server error",
+        });
+    }
+};
+
+export const getUserProfileController = async (req, res) => {
+    try {
+        const { userName } = req.params;
+        console.log(req.params);
+        const user = await userModel.findOne({ userName });
+
+        if (!user)
+            return res.status(404).json({
+                success: false,
+                message: "user not found",
+            });
+
+        return res.status(200).json({
+            success: true,
+            message: "user found successfully",
+            data: user,
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            message: "Internal server error",
+            error: error.message,
         });
     }
 };
