d9e5c3e39b9d5ee63b4fe68dce650dadc3b727ac
mausam <mausamkkushwaha@gmail.com>
2026-08-27 19:29:00 +0530

    add user-routes

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,11 +1,12 @@
 import express from "express"
 import { authMiddleware } from "../middlewares/auth.middleware.js"
-import { getMeController } from "../controllers/user.controller.js"
+import { getMeController, getUserProfileController } from "../controllers/user.controller.js"
 
 
 const router = express.Router()
 
 router.post("/get-me",authMiddleware,getMeController)
+router.get("/get-user/:userName",getUserProfileController)
 
 
 export default router
