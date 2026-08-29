ef23731c68fc40b4573d9fde52865125ba377d48
mausam <mausamkkushwaha@gmail.com>
2026-08-29 20:01:33 +0530

    feat : add update,delete-user functionality

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,53 +1,116 @@
 import userModel from "../models/user.model.js";
 
 export const getMeController = async (req, res) => {
-    try {
-        const user = await userModel.findById(req.user.id).select("     password")
+  try {
+    const user = await userModel.findById(req.user.id);
 
-        if (!user) {
-            return res.status(404).json({
-                success: false,
-                message: "user not found",
-            });
-        }
-
-        return res.status(200).json({
-            success: true,
-            message: "fetched myProfile",
-            data: user
-        });
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({
-            success: false,
-            message: "Internal server error",
-        });
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "user not found",
+      });
     }
+
+    return res.status(200).json({
+      success: true,
+      message: "fetched myProfile",
+      data: user,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+    });
+  }
 };
 
 export const getUserProfileController = async (req, res) => {
-    try {
-        const { userName } = req.params;
-        console.log(req.params);
-        const user = await userModel.findOne({ userName });
+  try {
+    const userId = req.params.id;
+    console.log(req.params);
+    const user = await userModel.findById(userId);
 
-        if (!user)
-            return res.status(404).json({
-                success: false,
-                message: "user not found",
-            });
+    if (!user)
+      return res.status(404).json({
+        success: false,
+        message: "user not found",
+      });
 
-        return res.status(200).json({
-            success: true,
-            message: "user found successfully",
-            data: user,
-        });
-    } catch (error) {
-        console.log(error);
-        return res.status(500).json({
-            success: false,
-            message: "Internal server error",
-            error: error.message,
-        });
+    return res.status(200).json({
+      success: true,
+      message: "user found successfully",
+      data: user,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+      error: error.message,
+    });
+  }
+};
+
+export const updateUserController = async (req, res) => {
+  try {
+    const { username, fullName, phone } = req.body;
+
+    const updateData = {};
+    if (username) {
+      updateData.username = username;
     }
+    if (fullName) {
+      updateData.fullname = fullname;
+    }
+    if (phone) {
+      updateData.phone = phone;
+    }
+
+    const updateUser = await userModel.findByIdAndUpdate(
+      req.user.id,
+      updateData,
+      {
+        new: true,
+      },
+    );
+
+    if (!updateUser) {
+      return res.status(404).json({
+        success: false,
+        message: "user not update",
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "user update successfully",
+      data: updateUser,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      message: "Internal server error",
+      error: error.message,
+    });
+  }
+};
+
+export const userDeleteController = async (req, res) => {
+  try {
+    const deleteUser = await userModel.findByIdAndDelete(req.user.id);
+
+    if (!deleteUser) {
+      return res.status(404).json({
+        success: false,
+        message: "user not found",
+      });
+    }
+
+    return res.status(200).json({
+      success: true,
+      message: "user deleted successfully",
+    });
+  } catch (error) { }
 };
--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -1,12 +1,14 @@
 import express from "express"
 import { authMiddleware } from "../middlewares/auth.middleware.js"
-import { getMeController, getUserProfileController } from "../controllers/user.controller.js"
+import { getMeController, getUserProfileController, updateUserController, userDeleteController } from "../controllers/user.controller.js"
 
 
 const router = express.Router()
 
 router.post("/get-me",authMiddleware,getMeController)
-router.get("/get-user/:userName",getUserProfileController)
+router.get("/get-user/:id",authMiddleware,getUserProfileController)
+router.patch("/update",authMiddleware,updateUserController)
+router.delete("/delete",authMiddleware,userDeleteController)
 
 
 export default router
