553a103c26ec48b549881e154643bcdc7e339a7b
mausam <mausamkkushwaha@gmail.com>
2026-09-08 13:30:18 +0530

    feat: role_model

---
+++ backend/src/models/roles.model.js
@@ -0,0 +1,39 @@
+import mongoose, { Types } from "mongoose";
+
+const role_Schema = new mongoose.Schema(
+  {
+    
+    name:{
+      type:String,
+      required:true
+    },
+
+    user:{
+      type:mongoose.Schema.Types.ObjectId
+    },
+
+    server: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "servers",
+    },
+
+    permissions: {
+      type: [String],
+      default: [],
+    },
+
+    position:{
+      type:Number,
+      default:0,
+    },
+
+    color: {
+      type: String,
+      default: "#dadada",
+    },
+  },
+  { timestamps: true },
+);
+
+const roleModel = mongoose.model("role",role_Schema);
+export default roleModel;
