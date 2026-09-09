72c5c60e20b7a432e0ba9bf4338e6d07d9b803b9
mausam <mausamkkushwaha@gmail.com>
2026-09-09 18:40:08 +0530

    feat add getServerMember functionality

--- backend/src/controllers/serverMember.controller.js
+++ backend/src/controllers/serverMember.controller.js
@@ -1,23 +1,24 @@
 import serverModel from "../models/server.model.js";
 import serverMember_Model from "../models/serverMember.model.js";
 import ApiError from "../utils/apiError.util.js";
+import ApiResponse from "../utils/apiResponse.util.js";
 
 export const get_ServerMember = async (req, res, next) => {
+
   try {
-    const { serverId } = req.params;
+    const serverId = req.params.id;
+    console.log(serverId);
 
     const server = await serverModel.findById(serverId);
+    console.log(server);
+    if(!server) throw new ApiError(404,"Server not found");
 
-    if (!server) throw new ApiError(404, "server not found or not exist");
-
-    const serverMember = await serverMember_Model
-      .find({ server: serverId })
-      .populate("user", "profile_pic")
-      .populate("roles", "name permissions color position");
-
-    console.log(serverMember);
+    const member = await serverMember_Model.find({server:serverId}).populate()
+    return res.status(200).json(
+      new ApiResponse(200,member,"Server member fetched successfully")
+    );
   } catch (error) {
-    console.log(error);
-    next(error);
+    console.log(error)
+    next(error)
   }
 };
