a3296f9540009f00b2d6ebb82469178611a56d8d
mausam <mausamkkushwaha@gmail.com>
2026-09-19 22:24:06 +0530

    channetUpdate functions

--- backend/src/controllers/channel.controller.js
+++ backend/src/controllers/channel.controller.js
@@ -40,11 +40,42 @@ export const getChannel = asyncHandler(async (req, res) => {
   }
 
   const channel = await channelModel.find({ server: serverId });
-  if (!channel) {
-    throw new ApiError(404, "channel not found");
+  if (channel === 0) {
+    throw new ApiError(404, "No channel found");
   }
 
   return res
     .status(200)
     .json(new ApiResponse(200, channel, "channels fetched successfully"));
+});
+
+export const updateChannel = asyncHandler(async (req, res) => {
+  const { serverId, channelId } = req.params;
+  const { channelName, channelType, isPrivate } = req.body;
+
+  const server = await serverModel.findById(serverId);
+  if (!server) throw new ApiError(404, "server not found");
+
+  const channel = await channelModel.findOne({
+    _id: channelId,
+    server: serverId,
+  });
+
+  if (!channel) {
+    throw new ApiError(404, "channel not found");
+  }
+
+  if (server.owner.toString() !== req.user._id.toString()) {
+    throw new ApiError(403, "only owner or admin and update channel");
+  }
+
+  if (channelName !== undefined) channel.channelName = channelName;
+  if (channelType !== undefined) channel.channelType = channelType;
+  if (isPrivate !== undefined) channel.isPrivate = isPrivate;
+
+  const updatedChannel = await channel.save();
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, updatedChannel, "Channel successfully updated"));
 });
