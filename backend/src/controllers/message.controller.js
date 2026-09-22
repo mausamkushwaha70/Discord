44a3ed8eaa9a3a2f27e522d9e38b887b5d7208e3
mausam <mausamkkushwaha@gmail.com>
2026-09-22 22:35:14 +0530

    feat: add getMessage functionality

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -2,87 +2,133 @@ import channelModel from "../models/channels.model";
 import messageModel from "../models/message.model";
 import { sendFile } from "../services/storage.service";
 
-
 export const createMessage = async (req, res, next) => {
-    try {
-        const { channelId } = req.params;
-        const { content } = req.body;
+  try {
+    const { channelId } = req.params;
+    const { content } = req.body;
 
-        const channel = await channelModel.findById(channelId);
+    const channel = await channelModel.findById(channelId);
 
-        if (!channel) {
-            throw new ApiError(404, "Channel not found");
-        }
-
-        const member = await serverModel.findOne({
-            server: channel.server,
-            user: req.user._id
-        });
-
-        if (!member) {
-            throw new ApiError(
-                403,
-                "You are not a member of this server"
-            );
-        }
-
-        // Check if message has either text or attachments
-        if (
-            (!content || !content.trim()) &&
-            (!req.files || req.files.length === 0)
-        ) {
-            throw new ApiError(
-                400,
-                "Message must contain text or an attachment"
-            );
-        }
-
-        // Upload attachments
-        let attachments = [];
-
-        if (req.files && req.files.length > 0) {
-            attachments = await Promise.all(
-                req.files.map(async (file) => {
-                    const uploadedFile = await sendFile(
-                        file.buffer,
-                        file.originalname
-                    );
-
-                    return {
-                        url: uploadedFile.url,
-                        type: file.mimetype.startsWith("image/")
-                            ? "image"
-                            : file.mimetype.startsWith("video/")
-                            ? "video"
-                            : "file",
-                        name: file.originalname
-                    };
-                })
-            );
-        }
-
-        const message = await messageModel.create({
-            content: content?.trim() || "",
-            author_id: req.user._id,
-            channel_id: channelId,
-            attachments
-        });
-
-        const messageDetails = await messageModel.findById(message._id).populate("author_id","profile_pic username")
-
-       const io = getIO()
-
-       io.to(`channel:${channelId}`).emit(
-        "message:new",messageDetails
-       )
-        return res.status(201).json(
-            new ApiResponse(
-                201,
-               "message",
-                "Message created successfully"
-            )
-        );
-    } catch (error) {
-        next(error);
+    if (!channel) {
+      throw new ApiError(404, "Channel not found");
     }
+
+    const member = await serverModel.findOne({
+      server: channel.server,
+      user: req.user._id,
+    });
+
+    if (!member) {
+      throw new ApiError(403, "You are not a member of this server");
+    }
+
+    // Check if message has either text or attachments
+    if (
+      (!content || !content.trim()) &&
+      (!req.files || req.files.length === 0)
+    ) {
+      throw new ApiError(400, "Message must contain text or an attachment");
+    }
+
+    // Upload attachments
+    let attachments = [];
+
+    if (req.files && req.files.length > 0) {
+      attachments = await Promise.all(
+        req.files.map(async (file) => {
+          const uploadedFile = await sendFile(file.buffer, file.originalname);
+
+          return {
+            url: uploadedFile.url,
+            type: file.mimetype.startsWith("image/")
+              ? "image"
+              : file.mimetype.startsWith("video/")
+                ? "video"
+                : "file",
+            name: file.originalname,
+          };
+        }),
+      );
+    }
+
+    const message = await messageModel.create({
+      content: content?.trim() || "",
+      author_id: req.user._id,
+      channel_id: channelId,
+      attachments,
+    });
+
+    const messageDetails = await messageModel
+      .findById(message._id)
+      .populate("author_id", "profile_pic username");
+
+    const io = getIO();
+
+    io.to(`channel:${channelId}`).emit("message:new", messageDetails);
+    return res
+      .status(201)
+      .json(new ApiResponse(201, "message", "Message created successfully"));
+  } catch (error) {
+    next(error);
+  }
+};
+
+const getAllChannelMessage = async (req, res, next) => {
+  try {
+    const { channelId } = req.params;
+
+    const channel = await channelModel.findById(channelId);
+
+    if (!channel) {
+      throw new ApiError(404, "Channel not found");
+    }
+
+    const member = await serverMemberModel.findOne({
+      server: channel.server,
+      user: req.user._id,
+    });
+
+    if (!member) {
+      throw new ApiError(403, "You are not a member of this server");
+    }
+
+    const messages = await messageModel.aggregate([
+      {
+        $match: {
+          channel_id: channel._id,
+        },
+      },
+      {
+        $lookup: {
+          from: "users",
+          localField: "author_id",
+          foreignField: "_id",
+          as: "author_details",
+        },
+      },
+      {
+        $unwind: "$author_details",
+      },
+      {
+        $project: {
+          content: 1,
+          createdAt: 1,
+
+          "author_details.username": 1,
+        },
+      },
+
+      {
+        $sort: {
+          createdAt: -1,
+        },
+      },
+    ]);
+
+    return res
+      .status(200)
+      .json(new ApiResponse(200, messages, "message fetched successfully"));
+  } catch (error) {
+    next(error);
+  }
 };
